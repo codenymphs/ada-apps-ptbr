cd718efb909586c0a4186127168ab4c52ee75472
juniobash <juniobash@outlook.com>
2022-08-17 11:26:09 -0300

    update programa

--- ada_playground.adb
+++
@@ -1,7 +0,0 @@
-WITH Text_io; 
-USE Text_io;
-
-PROCEDURE ada_playground IS
-BEGIN
-        Put_Line("Ola, Mundo!");
-END ada_playground;
---
+++ playground.adb
@@ -0,0 +1,7 @@
+WITH Text_io; 
+USE Text_io;
+
+PROCEDURE Playground IS
+BEGIN
+        Put_Line("Ada Said: Hello, World!");
+END Playground;
